16e70ee2608de98646a373b53248fb44d126157e
Minjung Kim <Reny.atkins@gmail.com>
2017-10-19 10:52:59 +0900

    이동 재현 - 기본적인 부분만

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Player : MonoBehaviour {
 
@@ -8,6 +9,14 @@ public class Player : MonoBehaviour {
 	public float slowCoef;
 	public float shotDelay;
 	float lastShotTime;
+	float lastCheckTrailTime;
+	public float trailInterval;
+	float currentTime = 0;
+
+	// 기록 방지를 위한 임시 변수.
+	bool isTrailOn = true;
+
+	Dictionary<float, Vector3> moveTrails;
 
 	public GameObject bullet;
 
@@ -37,12 +46,47 @@ public class Player : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		lastShotTime = 0;
+
+		lastCheckTrailTime = 0;
+		moveTrails = new Dictionary<float, Vector3>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		currentTime += Time.deltaTime;
+		if (isTrailOn)
+			CheckTrail();
 		Move();
 		Shot();
+
+		if (Input.GetKeyDown(KeyCode.P)) {
+			StartCoroutine (MoveByTrail());
+		}
+	}
+
+	// 슬로우 여부와 공격 여부도 추후 추가할 것.
+	void CheckTrail() {
+		lastCheckTrailTime += Time.deltaTime;
+
+		if (lastCheckTrailTime < trailInterval) return;
+
+		moveTrails.Add(currentTime, transform.position);
+		// Debug.Log("Time : " + currentTime + ", Pos : " + (Vector2)transform.position);
+		lastCheckTrailTime = 0;
+	}
+
+	IEnumerator MoveByTrail() {
+		isTrailOn = false;
+
+		foreach (var trail in moveTrails) {
+			if (trail.Key == 0) {
+				transform.position = trail.Value;
+			}
+			else {
+				transform.DOMove(trail.Value, trailInterval);
+			}
+			yield return new WaitForSeconds(trailInterval);
+		}
 	}
 
 	void Shot() {
