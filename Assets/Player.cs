3f55b858559dc8b48aa94f05c6a951cc71714f44
Minjung Kim <Reny.atkins@gmail.com>
2017-11-03 15:28:32 +0900

    입력 기록 프레임 단위로 바꿈

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -11,18 +11,22 @@ public class Player : MonoBehaviour {
 	float lastShotTime;
 
 	// 기록용
-	float lastCheckTrailTime;
-	public float trailInterval;
-	float currentTime = 0;
+	// float lastCheckTrailTime;
+	// public float trailInterval;
+	// float currentTime = 0;
+	int currentFrame = 0;
 
 	// 기록 방지를 위한 임시 변수.
 	bool isTrailOn = true;
 
-	Dictionary<float, Vector3> moveTrails;
+	// Dictionary<float, Vector3> moveTrails;
+	List<List<KeyCode>> moveInput;
+	List<bool> shotInput;
+	List<bool> slowInput;
 
 	public GameObject bullet;
 
-	bool isSlow() {
+	bool IsSlow() {
 		return Input.GetKey(KeyCode.LeftShift);
 	}
 
@@ -49,15 +53,23 @@ public class Player : MonoBehaviour {
 	void Start () {
 		lastShotTime = 0;
 
-		lastCheckTrailTime = 0;
-		moveTrails = new Dictionary<float, Vector3>();
+		// lastCheckTrailTime = 0;
+		// moveTrails = new Dictionary<float, Vector3>();
+
+		moveInput = new List<List<KeyCode>>();
+		shotInput = new List<bool>();
+		slowInput = new List<bool>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		currentTime += Time.deltaTime;
+	void FixedUpdate () {
+		// currentTime += Time.deltaTime;
+		currentFrame += 1;
 		if (isTrailOn)
 			CheckTrail();
+
+		slowInput.Add(IsSlow());
+
 		Move();
 		Shot();
 
@@ -68,36 +80,84 @@ public class Player : MonoBehaviour {
 
 	// 슬로우 여부와 공격 여부도 추후 추가할 것.
 	void CheckTrail() {
-		lastCheckTrailTime += Time.deltaTime;
+		// lastCheckTrailTime += Time.deltaTime;
 
-		if (lastCheckTrailTime < trailInterval) return;
+		// if (lastCheckTrailTime < trailInterval) return;
 
-		moveTrails.Add(currentTime, transform.position);
-		// Debug.Log("Time : " + currentTime + ", Pos : " + (Vector2)transform.position);
-		lastCheckTrailTime = 0;
+		// moveTrails.Add(currentTime, transform.position);
+		// // Debug.Log("Time : " + currentTime + ", Pos : " + (Vector2)transform.position);
+		// lastCheckTrailTime = 0;
 	}
 
 	IEnumerator MoveByTrail() {
 		isTrailOn = false;
 
-		foreach (var trail in moveTrails) {
-			if (trail.Key == 0) {
-				transform.position = trail.Value;
+		// foreach (var trail in moveTrails) {
+		// 	if (trail.Key == 0) {
+		// 		transform.position = trail.Value;
+		// 	}
+		// 	else {
+		// 		transform.DOMove(trail.Value, trailInterval).SetEase(Ease.Linear);
+		// 	}
+		// 	yield return new WaitForSeconds(trailInterval);
+		// }
+
+		for (int frame = 0; frame < moveInput.Count; frame++) {
+			// move
+			float speed = defaultSpeed;
+			List<KeyCode> inputs = moveInput[frame];
+
+			// shift키 입력시 저속이동
+			if (slowInput[frame]) 
+				speed *= slowCoef;
+
+			// 대각선으로 이동할 경우 속도 일정하게 유지
+			if ((inputs.Contains(KeyCode.LeftArrow) && inputs.Contains(KeyCode.UpArrow)) ||
+				(inputs.Contains(KeyCode.LeftArrow) && inputs.Contains(KeyCode.DownArrow)) ||
+				(inputs.Contains(KeyCode.RightArrow) && inputs.Contains(KeyCode.UpArrow)) ||
+				(inputs.Contains(KeyCode.RightArrow) && inputs.Contains(KeyCode.DownArrow))) {
+				speed *= 1/Mathf.Sqrt(2);
 			}
-			else {
-				transform.DOMove(trail.Value, trailInterval).SetEase(Ease.Linear);
+
+			if (inputs.Contains(KeyCode.LeftArrow)) {
+				transform.position += Vector3.left * Time.deltaTime * speed;
 			}
-			yield return new WaitForSeconds(trailInterval);
+			if (inputs.Contains(KeyCode.RightArrow)) {
+				transform.position += Vector3.right * Time.deltaTime * speed;
+			}
+			if (inputs.Contains(KeyCode.UpArrow)) {
+				transform.position += Vector3.up * Time.deltaTime * speed;
+			}
+			if (inputs.Contains(KeyCode.DownArrow)) {
+				transform.position += Vector3.down * Time.deltaTime * speed;
+			}
+
+			// shot
+			lastShotTime += Time.deltaTime;
+
+			if ((shotInput[frame]) && (lastShotTime > shotDelay)) {
+				if (slowInput[frame]) {
+					ShotStraightStrong();
+				}
+				else {
+					ShotSector();
+				}
+				lastShotTime = 0;
+			}
+
+			yield return null;
 		}
 	}
 
 	void Shot() {
 		lastShotTime += Time.deltaTime;
 
+		shotInput.Add(Input.GetKey(KeyCode.Z));
+
 		if (!Input.GetKey(KeyCode.Z)) return;
 		if (lastShotTime < shotDelay) return;
 
-		if (isSlow()) {
+		if (IsSlow()) {
 			ShotStraightStrong();
 		}
 		else {
@@ -110,7 +170,7 @@ public class Player : MonoBehaviour {
 		float speed = defaultSpeed;
 		
 		// shift키 입력시 저속이동
-		if (isSlow()) 
+		if (IsSlow()) 
 			speed *= slowCoef;
 
 		// 대각선으로 이동할 경우 속도 일정하게 유지
@@ -121,17 +181,25 @@ public class Player : MonoBehaviour {
 			speed *= 1/Mathf.Sqrt(2);
 		}
 
+		List<KeyCode> inputs = new List<KeyCode>();
+
 		if (Input.GetKey(KeyCode.LeftArrow)) {
 			transform.position += Vector3.left * Time.deltaTime * speed;
+			inputs.Add(KeyCode.LeftArrow);
 		}
 		if (Input.GetKey(KeyCode.RightArrow)) {
 			transform.position += Vector3.right * Time.deltaTime * speed;
+			inputs.Add(KeyCode.RightArrow);
 		}
 		if (Input.GetKey(KeyCode.UpArrow)) {
 			transform.position += Vector3.up * Time.deltaTime * speed;
+			inputs.Add(KeyCode.UpArrow);
 		}
 		if (Input.GetKey(KeyCode.DownArrow)) {
 			transform.position += Vector3.down * Time.deltaTime * speed;
+			inputs.Add(KeyCode.DownArrow);
 		}
+
+		moveInput.Add(inputs);
 	}
 }
