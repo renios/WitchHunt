26c3dfaac25dec53ac0e8985442ccf472a44c06c
Minjung Kim <Reny.atkins@gmail.com>
2017-11-09 04:29:20 +0900

    피격시 화면 세로 1/4를 반경으로 하는 영역의 적 탄 소거

--- Assets/PlayerCollider.cs
+++ Assets/PlayerCollider.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class PlayerCollider : MonoBehaviour {
 
@@ -19,7 +20,18 @@ public class PlayerCollider : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.tag == "EnemyBullet") {
 			player.Damaged();
-			Destroy(other.gameObject);
+
+			Collider2D[] enemyBulletColliders = Physics2D.OverlapCircleAll(transform.position, Camera.main.orthographicSize / 2.0f);
+			enemyBulletColliders.ToList().ForEach(coll => {
+				if (coll.tag == "EnemyBullet") {
+					Destroy(coll.gameObject);
+				}
+			});
 		}
 	}
+
+	//public static Collider2D[] OverlapCircleAll(Vector2 point, 
+	//	float radius, int layerMask = DefaultRaycastLayers, 
+	//	float minDepth = -Mathf.Infinity, float maxDepth = Mathf.Infinity);
+
 }
