dd6aeef4b4da881a059be6ddf7da1cbc69122842
Minjung Kim <Reny.atkins@gmail.com>
2017-12-08 05:25:31 +0900

    2스테이지 배경 스크롤

---
+++ Assets/Script/Background.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+
+	public float scrollSpeed = 0.5f; 
+	float offset;
+
+    void Start () {
+    
+	}
+
+    void Update () {
+		offset += Time.deltaTime * scrollSpeed;
+		GetComponent<Renderer>().material.mainTextureOffset = new Vector2(offset, 0);		
+    }
+}
