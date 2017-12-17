ddf8923cf7dbd71950391208098430db7fd87176
AirHorizons <guardian1248@snu.ac.kr>
2017-12-18 03:09:19 +0900

    스테이지에 음악 달고 있습니다

---
+++ Assets/Script/ButtonManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public void MoveScene (string dest) {
+		if (dest == "start")
+			SceneManager.LoadScene("Stage1-A");
+		else if (dest == "continue")
+			SceneManager.LoadScene("StageSelect");
+		else if (dest == "exit")
+			Application.Quit();
+	}
+}
