14542910ea9ad4c7fdc197e5955ef1bb1663d851
Minjung Kim <Reny.atkins@gmail.com>
2017-12-18 08:35:54 +0900

    1-a 스테이지 폭탄 구현

---
+++ Assets/BombDamage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombDamage : MonoBehaviour {
+
+	public int damageRatio;
+	public int damageAmount;
+
+	// Use this for initialization
+	void Start () {
+		Boss boss = FindObjectOfType<Boss>();
+		
+		if (damageRatio > 0) {
+			int damage = (int)((damageRatio / 100f) * boss.maxHp);
+			boss.Damaged(damage);
+		}
+
+		if (damageAmount > 0) {
+			boss.Damaged(damageAmount);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/RemoveEnemyBullets.cs
+++ Assets/RemoveEnemyBullets.cs
@@ -7,20 +7,32 @@ public class RemoveEnemyBullets : MonoBehaviour {
 
 	float radius;
 	public float lifeTime;
+	Collider2D coll;
 
 	// Use this for initialization
 	void Start () {
-		radius = GetComponent<CircleCollider2D>().radius;
-
+		if (GetComponent<CircleCollider2D>() != null)
+			radius = GetComponent<CircleCollider2D>().radius;
+		
 		if (lifeTime != 0)
 			Destroy(gameObject, lifeTime);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Collider2D[] enemyBulletCollidersArray = Physics2D.OverlapCircleAll(transform.position, radius);
-		List<Collider2D> enemyBulletColliders = enemyBulletCollidersArray.ToList();
-
+		List<Collider2D> enemyBulletColliders = new List<Collider2D>();
+		
+		if (GetComponent<CircleCollider2D>() != null) {
+			Collider2D[] enemyBulletCollidersArray;
+			enemyBulletCollidersArray = Physics2D.OverlapCircleAll(transform.position, radius);
+			enemyBulletColliders = enemyBulletCollidersArray.ToList();
+		}	
+		else {			
+			List<GameObject> enemyBullets = GameObject.FindGameObjectsWithTag("EnemyBullet").ToList();
+			enemyBullets.ForEach(bullet => enemyBulletColliders.Add(bullet.GetComponent<Collider2D>()));
+		}
+			
+		
 		if (gameObject.tag == "MushroomBomb") {
 			enemyBulletColliders = enemyBulletColliders.FindAll(
 				coll => coll.GetComponent<MushroomBullet>() == null
