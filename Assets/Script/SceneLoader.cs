77680412eca9a1c1448021da4fba93126df0bcdb
AirHorizons <guardian1248@snu.ac.kr>
2017-12-19 14:29:28 +0900

    []키 눌렀을 때 2스테이지에선 2A, 2B로 이동하게 변경

--- Assets/Script/SceneLoader.cs
+++ Assets/Script/SceneLoader.cs
@@ -13,10 +13,16 @@ public class SceneLoader : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.LeftBracket)) {
-			SceneManager.LoadScene("Stage1-A");
+			if (SceneManager.GetActiveScene().name == "Stage1-B")
+				SceneManager.LoadScene("Stage1-A");
+			else if (SceneManager.GetActiveScene().name == "Stage2-B")
+				SceneManager.LoadScene("Stage2-A");
 		}
 		if (Input.GetKeyDown(KeyCode.RightBracket)) {
-			SceneManager.LoadScene("Stage1-B");
+			if (SceneManager.GetActiveScene().name == "Stage1-A")
+				SceneManager.LoadScene("Stage1-B");
+			else if (SceneManager.GetActiveScene().name == "Stage2-A")
+				SceneManager.LoadScene("Stage2-B");
 		}	
 
 		if (Input.GetKeyDown(KeyCode.R)) {
